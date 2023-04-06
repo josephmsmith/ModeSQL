644d31aac1347df46e16ea5ba846318e20a34143
Joseph Smith <joseph.smit95@gmail.com>
2023-04-06 12:41:14 -0700

    Add files via upload

--- learning_windows_functions.sql
+++ learning_windows_functions.sql
@@ -46,10 +46,107 @@ AND
 	| CURRENT ROW
 */
 
+-- Example 1
+SELECT species, name, 
+	MAX(name)
+	OVER (PARTITION BY species ORDER BY name 
+		 ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS number_of_animals
+FROM animals;
 
 
+-- show the number of animals admitted prior to the current day
+SELECT species, name, primary_color, admission_date,
+	avg heart_rate OVER (PARTITION BY species 
+		  ORDER BY admission_date asc 
+		 ROWS BETWEEN UNBOUNDED PRECEDING 
+		  AND 
+		  CURRENT ROW) AS upto_spc_animl
+FROM animals
+ORDER BY species asc, admission_date asc;
+
+-- FRAME EXCLUSIONS
+-- 4 Optional Clauses
+/*
+EXLUDE NO OTHERS
+EXCLUDE GROUP 
+EXCLUDE TIES 
+EXCLUDE CURRENT ROW
+
+-- NULL HANDLING
+- Aggregate functions ignore nulls
+- Rank and distribution respect NULLS
+- Frame and boundaries respect NULLS
+*/
+
+-- Practice
+-- return animal species, name, checkup time, hr, t/f when hr >= avg species hr
+
+select species, name, checkup_time, heart_rate, 
+	cast(avg(heart_rate)
+		 over(PARTITION BY species)
+		 as decimal (5,2)
+		) as species_Avg_hr
+from routine_checkups
+order by species asc, checkup_time asc;
+
+-- Monthly Adoption fee and that percent of the year
+-- select year, month, monthly revenue, percent of current year
+with monthly_grouped_adoptions as						--define the group query in CTE
+	(select date_part('year', adoption_date) as year, 
+			date_part('month', adoption_date) as month,
+			sum(adoption_fee) as mth_total
+	from adoptions
+	group by 1,2)
+select *,												--window function that defines percentage
+	cast	(100* mth_total								--longer but clearer for readability
+			/ SUM(mth_total)
+			 over(partition by year)
+			 as decimal(5,2)
+			) as annual_pecent
+from monthly_grouped_adoptions
+order by 1, 2;
 
 
+/* 
+----------------------------------------------------
+-- Warm up challenge - Annual vaccinations report --
+----------------------------------------------------
 
+Write a query that returns all years in which animals were vaccinated, and the total number of vaccinations 
+given that year.
+In addition, the following two columns should be included in the results:
+1. The average number of vaccinations given in the previous two years.
+2. The percent difference between the current year's number of vaccinations, and the average of the previous two years.
+For the first year, return a NULL for both additional columns.
 
+Hint: Cast averages and division expressions to DECIMAL (5, 2)
+
+Expected result sorted by year ASC:
+---------------------------------------------------------------------------------------------
+|	year	|	number_of_vaccinations	|	previous_2_years_average	|	percent_change	|
+|-----------|---------------------------|-------------------------------|-------------------|
+|	2,016	|					11		|					[NULL]		|		[NULL]		|
+|	2,017	|					23		|					11.00		|		209.09		|
+|	2,018	|					32		|					17.00		|		188.24		|
+|	2,019	|					29		|					27.50		|		105.45		|
+---------------------------------------------------------------------------------------------
+*/
+
+-- first figure out annual vaccainations with first CTE, then add 2nd CTE with previous 2yr avg
+with 
+vac_by_year as (
+	select date_part('year', vaccination_time)::INT as vac_year, count(*) as number_of_vaccinations 
+	from vaccinations
+	group by 1 ),
+annual_vac_2yr_avg as
+	(select *, cast( avg(number_of_vaccinations)
+					over(order by vac_year asc range between 2 preceding and 1 preceding)
+					as decimal(5,2)) as prev_2yr_avg
+	 from vac_by_year
+	) 
+select *,
+	(100*number_of_vaccinations/prev_2yr_avg)::Decimal(5,2) as pct_chg
+from annual_vac_2yr_avg order by vac_year asc;
+ 
+d
 
